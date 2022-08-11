ee28d5c44d7941166d866eb24787a52415a5a548
Alexandr Grisin <agrisin@griddynamics.com>
2022-08-11 17:20:03 +0300

    Cypress add login tests

---
+++ cypress-e2e/cypress/e2e/login.cy.js
@@ -0,0 +1,20 @@
+describe("Login spec", () => {
+  beforeEach(function () {
+    cy.fixture("users").then((users) => {
+      this.users = users;
+    });
+    cy.visit("/");
+  });
+
+  it("should log in", function () {
+    cy.login(this.users.testuser.username, this.users.testuser.password);
+    cy.get("[data-test=sidenav-username]")
+      .should("exist")
+      .contains(this.users.testuser.username);
+  });
+
+  it("should see log in error on invalid credentials", function () {
+    cy.login(this.users.invaliduser.username, this.users.invaliduser.password);
+    cy.get(".MuiAlert-message").should("exist");
+  });
+});
