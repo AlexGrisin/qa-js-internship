8e22e9321203c84c7b2142aaf00b93eee3ccf1da
Alexandr Grisin <agrisin@griddynamics.com>
2022-08-13 14:40:04 +0300

    Playwright add api tests

---
+++ playwright-e2e/tests/api/login.spec.js
@@ -0,0 +1,33 @@
+const { test, expect } = require("@playwright/test");
+const { testUser, nonExistingUser } = require("../../data/users");
+
+let apiContext;
+
+test.beforeAll(async ({ playwright }) => {
+  apiContext = await playwright.request.newContext({
+    baseURL: "http://localhost:3001",
+    extraHTTPHeaders: {},
+  });
+});
+
+test("should log in", async () => {
+  const loginResponse = await apiContext.post("/login", {
+    data: {
+      username: testUser.username,
+      password: testUser.password,
+      type: "LOGIN",
+    },
+  });
+  expect(loginResponse.ok()).toBeTruthy();
+});
+
+test("should see log in error on invalid credentials", async () => {
+  const loginResponse = await apiContext.post("/login", {
+    data: {
+      username: nonExistingUser.username,
+      password: nonExistingUser.password,
+      type: "LOGIN",
+    },
+  });
+  expect(loginResponse.ok()).toBeFalsy();
+});
