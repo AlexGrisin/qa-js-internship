6b794ab31e3954507a56d84740c5e82fe9f56d58
Alexandr Grisin <agrisin@griddynamics.com>
2022-08-29 10:40:21 +0300

    Add todo for proper mochawesome report declaration

--- cypress-e2e/cypress.config.js
+++ cypress-e2e/cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  // TODO: use 'mochawesome' declaration after issue https://github.com/cypress-io/cypress/issues/4536
   reporter: '../../node_modules/mochawesome/src/mochawesome.js',
   reporterOptions: {
     toConsole: true,
