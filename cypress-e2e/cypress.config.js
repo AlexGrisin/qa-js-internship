17944e83673c7e04de7412c23c3a7d3a31f62d8f
Alexandr Grisin <agrisin@griddynamics.com>
2022-08-29 10:38:54 +0300

    Cypress add mocha report

--- cypress-e2e/cypress.config.js
+++ cypress-e2e/cypress.config.js
@@ -1,6 +1,12 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  reporter: '../../node_modules/mochawesome/src/mochawesome.js',
+  reporterOptions: {
+    toConsole: true,
+    html: true,
+    json: true,
+  },
   e2e: {
     baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
