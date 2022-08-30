a6ec27a394512e638781af0b5437064b3126c33e
Alexandr Grisin <agrisin@griddynamics.com>
2022-08-30 09:35:56 +0300

    Cypress api tests

---
+++ cypress-e2e/cypress-api.config.js
@@ -0,0 +1,18 @@
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  // TODO: use 'mochawesome' declaration after issue https://github.com/cypress-io/cypress/issues/4536
+  reporter: '../../node_modules/mochawesome/src/mochawesome.js',
+  reporterOptions: {
+    toConsole: true,
+    html: true,
+    json: true,
+  },
+  e2e: {
+    baseUrl: 'http://localhost:3001',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    specPattern: 'cypress/integration/**/*.cy.{js,jsx,ts,tsx}',
+  },
+});
