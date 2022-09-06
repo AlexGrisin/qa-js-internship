1c4372b1a3b79179dd4e8cbcddf299f153ca20c1
Alexandr Grisin <alexander.grisin@gmail.com>
2022-09-06 14:56:21 +0300

    Cypress update supportfile

--- cypress-tests/cypress.config.js
+++ cypress-tests/cypress.config.js
@@ -12,6 +12,7 @@ module.exports = defineConfig({
   },
   e2e: {
     baseUrl: 'http://localhost:3000',
+    supportFile: 'cypress/support/index.js',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
